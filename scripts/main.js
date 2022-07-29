ef333b1a241882cc09c8872fc32b39498f4944bc
Davina Leong <davina.leong.sy@gmail.com>
2022-07-29 18:27:50 +0800

    Added function to calculate amounts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
+// Elements
 const tipForm = document.querySelector("#tipForm");
 const amountPerPerson = document.querySelector("#amountPerPerson");
 const totalPerPerson = document.querySelector("#totalPerPerson");
 const tipStatus = document.querySelector("#tipStatus");
 const resetButton = document.querySelector("#resetButton");
+
+// Form values
+let bill = 0;
+let tip = 1;
+let noOfPersons = 0;
 
 const resetForm = () => {
   tipForm.reset();
@@ -10,10 +16,21 @@ const resetForm = () => {
   totalPerPerson.innerHTML = `$0.00`;
 };
 
-tipForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+const calculateAmounts = () => {
+  const tipDecimal = numeral(tip / 100).format(`0.00`);
+  const tipAmount = numeral(bill * tipDecimal);
 
-resetButton.addEventListener("click", (e) => {
+  const tipPerPerson = numeral(tipAmount / noOfPersons).format(`0.00`);
+  const totalPerPerson = numeral((bill + tipAmount) / noOfPersons).format(
+    `0.00`
+  );
+
+  return {
+    tipPerPerson,
+    totalPerPerson,
+  };
+};
+
+resetButton.addEventListener(`click`, (e) => {
   resetForm();
 });
